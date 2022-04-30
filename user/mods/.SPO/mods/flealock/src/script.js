84e526bef50499fc7d39b6a405b13488b2fb23bb
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-04-29 22:47:21 -0400

    fix syntax
    somehow while editing this last time on my phone it jumped and deleted a random chunk of the script
    
    github on mobile is doodoo

--- user/mods/.SPO/mods/flealock/src/script.js
+++ user/mods/.SPO/mods/flealock/src/script.js
@@ -40,7 +40,7 @@ class tweakstuff
         if (pmcData.Hideout.Areas[11].level == 0)
         {
             globals.RagFair.minUserLevel = 90
-            locales.en.interface["ragfair/Unlocked at character LVL {0}"] = "
+            locales.en.interface["ragfair/Unlocked at character LVL {0}"] = "The Intelligence Center must be installed in your Hideout in order to communicate with the Flea Market network."
         }
         
         if (pmcData.Hideout.Areas[4].active && pmcData.Hideout.Areas[11].level > 0)
